9b3b6b65a4b7af353e20d4454cdd3dd354ba91a8
rafapaz <rafapaz@gmail.com>
2018-11-01 08:58:23 -0300

    Raffle details and edit

--- rifa/views.py
+++ rifa/views.py
@@ -1,15 +1,18 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from .forms import RaffleForm
+from .models import Raffle
 import datetime
 
 
 def index(request):
     if not request.user.is_authenticated:
-        return redirect('login') 
+        return redirect('login')
 
-    return render(request, 'rifa/index.html')
+    raffles = Raffle.objects.all()
+
+    return render(request, 'rifa/index.html', {'raffles': raffles})
 
 
 def my_login(request):
@@ -18,7 +21,7 @@ def my_login(request):
         psw = request.POST.get('psw')
 
         if email is None and psw is None:
-            return render(request, 'rifa/login.html')
+            return render(request, 'login.html')
 
         username = User.objects.get(email=email.lower()).username
         user = authenticate(username=username, password=psw)
@@ -30,7 +33,7 @@ def my_login(request):
         return redirect('index')
     except:
         context = {'message': 'Email or password not valid'}
-        return render(request, 'rifa/login.html', context)
+        return render(request, 'login.html', context)
 
 
 def my_logout(request):
@@ -38,10 +41,10 @@ def my_logout(request):
     return redirect('login')
 
 
-def create_raffle(request):
+def raffle_new(request):
     if not request.user.is_authenticated:
-        return redirect('login') 
-    
+        return redirect('login')
+
     if request.method == "POST":
         form = RaffleForm(request.POST)
 
@@ -53,4 +56,27 @@ def create_raffle(request):
             return redirect('index')
 
     form = RaffleForm()
-    return render(request, 'rifa/edit_raffle.html', {'form': form, 'msg': form.errors})
+    return render(request, 'rifa/raffle_edit.html', {'form': form, 'msg': form.errors})
+
+
+def raffle_edit(request, pk):
+    raffle = get_object_or_404(Raffle, pk=pk)
+    if request.method == "POST":
+        form = RaffleForm(request.POST, instance=raffle)
+
+        if form.is_valid():
+            raffle = form.save(commit=False)
+            raffle.author = request.user
+            raffle.published_date = datetime.datetime.now()
+            raffle.save()
+            return redirect('index')
+    else:
+        form = RaffleForm(instance=raffle)
+
+    return render(request, 'rifa/raffle_edit.html', {'form': form})
+
+
+def raffle_detail(request, pk):
+    raffle = get_object_or_404(Raffle, pk=pk)
+    context = {'raffle': raffle, 'range': range(1, raffle.qtd_num + 1)}
+    return render(request, 'rifa/raffle_detail.html', context)
